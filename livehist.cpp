1826df90a5e11cca8715da85172d926a7084ed1d
Michael Schwarz <michael.schwarz91@gmail.com>
2019-07-25 22:22:26 +0200

    more options

--- livehist.cpp
+++ livehist.cpp
@@ -7,6 +7,7 @@
 #include <cctype>
 #include <locale>
 #include <vector>
+#include <chrono>
 #include <memory.h>
 #include <unistd.h>
 #include <getopt.h>
@@ -19,6 +20,8 @@ static getopt_arg_t options[] =
                 {"no-clear",  no_argument,       NULL, 'c', "Disable screen clear.", NULL},
                 {"reverse",   no_argument,       NULL, 'r', "Short ascending.",      NULL},
                 {"percent",   no_argument,       NULL, 'p', "Show frequency as percentage.", NULL},
+                {"draw",      no_argument,       NULL, 'd', "Draw bar for visualization.", NULL},
+                {"interval",  required_argument, NULL, 'i', "Refresh interval (default: 0).", NULL},
                 {"switch",    no_argument,       NULL, 'x', "Show count first, then value.", NULL},
                 {"help",      no_argument,       NULL, 'h', "Show this help.",       NULL},
                 {NULL, 0,                        NULL, 0, NULL,                      NULL}
@@ -62,7 +65,7 @@ static inline void trim(std::string &s) {
 }
 
 std::map<std::string, size_t> hist;
-typedef std::function<bool(std::pair<std::string, size_t>, std::pair<std::string, size_t>)> Comparator;
+std::vector<std::pair<std::string, size_t>> pairs;
 
 bool sorter(std::pair<std::string, size_t>& elem1, std::pair<std::string, size_t>& elem2) {
     return elem1.second > elem2.second;
@@ -71,14 +74,33 @@ bool rev_sorter(std::pair<std::string, size_t>& elem1, std::pair<std::string, si
     return elem1.second < elem2.second;
 }
 
+size_t update(std::string& line) {
+    // update histogram
+    trim(line);
+    if(hist.find(line) != hist.end()) {
+        hist[line]++;
+    } else {
+        hist[line] = 1;
+    }
+
+    size_t sum = 0;
+    pairs.clear();
+    for (auto itr = hist.begin(); itr != hist.end(); ++itr) {
+        pairs.push_back(*itr);
+        sum += itr->second;
+    }
+    return sum;
+}
+
 int main(int argc, char* argv[]) {
     std::string seperator = " :: ";
-    int clear = 1, reverse = 0, order = 0, percent = 0;
-    size_t number = 0;
+    int clear = 1, reverse = 0, order = 0, percent = 0, draw = 0;
+    float interval = 0;
+    size_t number = 0, last_update = 0;
     
     struct option *long_options = getopt_get_long_options((getopt_arg_t *) options);
     int c;
-    while ((c = getopt_long(argc, argv, ":s:chrn:xp", long_options, NULL)) != EOF) {
+    while ((c = getopt_long(argc, argv, ":s:chrn:xpdi:", long_options, NULL)) != EOF) {
         switch (c) {
             case 's':
                 seperator = std::string(optarg);
@@ -97,6 +119,12 @@ int main(int argc, char* argv[]) {
                 break;
             case 'p':
                 percent = 1;
+                break;
+            case 'd':
+                draw = 1;
+                break;
+            case 'i':
+                interval = strtof(optarg, NULL);
                 break;
             case 'n':
                 number = strtoul(optarg, NULL, 0);
@@ -126,45 +154,60 @@ int main(int argc, char* argv[]) {
     free(long_options);    
     
     for (std::string line; getline(std::cin, line);) {
-        trim(line);
-        if(hist.find(line) != hist.end()) {
-            hist[line]++;
-        } else {
-            hist[line] = 1;
-        }
-        
-        std::vector<std::pair<std::string, size_t>> pairs;
-        for (auto itr = hist.begin(); itr != hist.end(); ++itr) {
-            pairs.push_back(*itr);
-        }
+        size_t sum = update(line);
+        if(!sum) sum = 1;
 
+        // sort bins
         sort(pairs.begin(), pairs.end(), reverse ? rev_sorter : sorter);
     
-        if(clear) {
-            std::cout << "\033[2J";
-        } else {
-            std::cout << std::endl;
-        }
-        
-        size_t sum = 0;
-        if(percent) {
-            for(auto& k : hist) {
-                sum += k.second;
-            }
-        }
-        if(!sum) sum = 1;
-        
-        size_t limit = number;
-        for (std::pair<std::string, size_t> element : pairs) {
-            if(number && !limit) {
-                break;
-            }
-            limit--;
-            if(order) {
-                std::cout << element.first << seperator << (percent ? element.second * 100.0 / sum : element.second) << std::endl;
+        // output
+        size_t now = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+
+        if(!interval || now - last_update >= interval * 1000) {
+            last_update = now;
+
+            if(clear) {
+                std::cout << "\033[2J";
             } else {
-                std::cout << (percent ? element.second * 100.0 / sum : element.second) << seperator << element.first << std::endl;
+                std::cout << std::endl;
             }
-        }    
+
+            size_t maxlen = 0;
+            for(int output = 0; output < 2; output++) {
+                char buffer[256], tmp[256];
+                size_t limit = number;
+                for (std::pair<std::string, size_t> element : pairs) {
+                    if(number && !limit) {
+                        break;
+                    }
+                    limit--;
+                    size_t len = 0;
+                    if(order) {
+                        len += snprintf(buffer, sizeof(buffer), "%s%s", element.first.c_str(), seperator.c_str());
+                        if(percent) len += snprintf(tmp, sizeof(tmp), "%f", element.second * 100.0 / sum);
+                        else len += snprintf(tmp, sizeof(tmp), "%zd", element.second);
+                        strncat(buffer, tmp, sizeof(buffer));
+                    } else {
+                        if(percent) len += snprintf(buffer, sizeof(buffer), "%f", element.second * 100.0 / sum);
+                        else len += snprintf(buffer, sizeof(buffer), "%zd", element.second);
+                        len += snprintf(tmp, sizeof(tmp), "%s%s", seperator.c_str(), element.first.c_str());
+                        strncat(buffer, tmp, sizeof(buffer));
+                    }
+                    if(len > maxlen) maxlen = len;
+    //                 snprintf(tmp, sizeof(tmp), "%s\n", buffer);
+                    if(output) {
+                        len = printf("%s", buffer);
+                        if(draw) {
+                            while(len++ < maxlen) {
+                                printf(" ");
+                            }
+                            printf(" |");
+                            for(unsigned int i = 0; i < ((120 - maxlen) * element.second) / sum; i++) printf("#");
+                        }
+                        printf("\n");
+                    }
+                }
+            }
+        }
     }
 }
